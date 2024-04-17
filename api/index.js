ad56c69a0bc68b4b95f2dce5b58c45f2a17f068a
Valeria Serci <balerty84@gmail.com>
2024-04-17 14:45:22 +0100

    update the message to test the deployment working properly

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello world, this is Craft-Purse-Backend')
+  res.send('Hello world, this is Craft-Purse-Backend updated')
 });
 
 app.listen(3000, () => {
