ce1cfd5d99a18f0953c5503c7e6e0fe59305391e
Valeria Serci <balerty84@gmail.com>
2024-04-17 11:28:19 +0100

    display a message when visiting localhost 8080

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello world, this is Craft-Purse-Backend')
+});
 
 app.listen(8080, () => {
-  console.log('running');
+  console.log(`Server is running at http://localhost:${port}`);
 })
