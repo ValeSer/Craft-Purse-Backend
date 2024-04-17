699f24cd27f1b251fcab38712d6a4f5440bd775b
Valeria Serci <balerty84@gmail.com>
2024-04-17 12:14:16 +0100

    configure to vercel for deployment

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello world, this is Craft-Purse-Backend')
 });
 
-app.listen(8080, () => {
+app.listen(3000, () => {
   console.log(`Server is running at http://localhost:${port}`);
 })
 
