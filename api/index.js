7f0702c2a51346da98a8990e51ef889997633259
Valeria Serci <balerty84@gmail.com>
2024-04-30 22:17:15 +0100

    test call to database

--- api/index.js
+++ api/index.js
@@ -7,8 +7,19 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello world, this is Craft-Purse-Backend updated')
+app.get('/', async(req, res) => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const db = await client.db("Craft-Purse-Db");
+    const materials = await db.collection("Materials-test").find({}).toArray()
+    
+    res.send(materials[0].name);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 });
 
 app.listen(3000, () => {
@@ -28,18 +39,7 @@ const client = new MongoClient(uri,  {
 );
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    const db = await client.db("Craft-Purse-Db");
-    const materials = await db.collection("Materials-test").find({}).toArray()
-    
-    console.log(materials[0].name);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  
 }
 run().catch(console.dir);
 module.exports = app
