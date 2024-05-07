49368a028b88b0a02cc2aa021e04fd28b2fca9cf
Valeria Serci <balerty84@gmail.com>
2024-05-07 17:31:35 +0100

    use mongoose for connection

--- api/index.js
+++ api/index.js
@@ -4,22 +4,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const Inventory = require('./models/inventory');
 
 app.use(cors());
 
 app.get('/', async(req, res) => {
   try {
-    const db = await client.db("Craft-Purse-Db");
-    const materials = await db.collection("Materials-test").find({}).toArray()
-    
-    res.send(materials[0].name);
+    console.log('riga13')
+    // const db = await client.db("Craft-Purse-Db");
+    // const materials = await db.collection("Materials-test").find({}).toArray()
+    const inventory = await Inventory.find({})
+    console.log('riga17')
+    //res.send(materials[0].name);
+    res.status(200).json(inventory)
   } catch(e){
-    res.send('error')
+    console.log(e)
+    res.send(e)
   } 
 });
-
-app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+client().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  })
 })
+
 
 module.exports = app
