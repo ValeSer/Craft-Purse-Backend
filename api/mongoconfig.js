3f70c56de814e2f827b5eaa62b4a59476ab65b14
Valeria Serci <balerty84@gmail.com>
2024-05-07 17:32:36 +0100

    use mongoose

--- api/mongoconfig.js
+++ api/mongoconfig.js
@@ -1,14 +1,31 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.DB_URL;
+require('dotenv').config();
+const mongoose = require('mongoose');
+//const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.DB_URL || "mongodb://0.0.0.0/craftPurse";
 
-const client = new MongoClient(uri,  {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(uri,  {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-client.connect();
+// client.connect();
 
-module.exports = client
+// module.exports = client
+
+const client = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    return db;
+ } catch (error) {
+  console.log('Unable to onnect to MongoDB', error);
+  throw error;
+ }
+};
+
+module.exports = client;
