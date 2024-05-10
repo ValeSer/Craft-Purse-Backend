47dafaeb82202045007e198ef9c7988205d30024
Valeria Serci <balerty84@gmail.com>
2024-05-10 12:34:09 +0100

    clean the file

--- api/mongoconfig.js
+++ api/mongoconfig.js
@@ -1,19 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-//const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri = process.env.DB_URL || "mongodb://0.0.0.0/craftPurse";
-
-// const client = new MongoClient(uri,  {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// client.connect();
-
-// module.exports = client
 
 const client = async () => {
   try {
