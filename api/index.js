64495bd7d8f87aed4f466396269ab6759cdbb40b
Valeria Serci <balerty84@gmail.com>
2024-05-17 14:38:58 +0100

    tidy up file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Material = require('./models/material')
 
+const Material = require('./models/material')
 const MaterialController = require('./controllers/material')
+
 app.use(cors());
 app.use(express.json());
 
