b1516da9081fbed7c3bd4ab08cd6cb0930b014fc
Valeria Serci <balerty84@gmail.com>
2024-06-27 23:49:50 +0100

    password encryption

--- api/controllers/signUp.js
+++ api/controllers/signUp.js
@@ -1,18 +1,23 @@
 const User = require('../models/user')
+const bcrypt = require("bcryptjs")
+
 
 const SignUpController = {
-  SignUp: (req, res) => {
+  SignUp: async (req, res) => {
+    try {
     const user = new User()
     user.username = req.body.username
-    user.password = req.body.password
-    user.save().then(() => {
+    user.password = await encryptPassword(req.body.password)
+    await user.save()
       res.sendStatus(201)
-    })
-    .catch(e => {
+    } catch(e) {
       console.log(e)
       res.sendStatus(400)
-    })
+    }
   }
 }
+const encryptPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
 
 module.exports = SignUpController;
